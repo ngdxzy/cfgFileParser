077c74f651d26aef4ba17634fc08d700e5cc58ee
Zhenyu Xu <zxu3@clemson.edu>
2024-02-06 16:30:17 -0500

    InitWorks
    Now it should works;

--- parser.py
+++ parser.py
@@ -308,12 +308,17 @@ class parser:
         sourceFile.close()
 
 if __name__ == '__main__':
-    a = parser()
-    a.addArgument("observation_point", "int int float float", "obs_row obs_col golden weight", [1, 1, 1, 1], 0)
-    a.addArgument("src_point", "int int", "src_row src_col", [1, 1], 0)
-    a.addArgument("nrow", "int", "nrow", [11], 1)
-    a.addArgument("ncol", "int", "ncol", [12], 1)
-    a.addArgument("binFileName", "string", "binFileName", ["c.bin"], 1)
-    a.generateC()
-    cfg = a.interpret('demo.cfg')
+    cfgParser = parser()
+    cfgParser.addArgument("observation_point", "int int float float", "obs_row obs_col golden weight", [1, 1, 1, 1], 0)
+    cfgParser.addArgument("src_point", "int int", "src_row src_col", [1, 1], 0)
+    cfgParser.addArgument("nrow", "int", "nrow", [11], 1)
+    cfgParser.addArgument("ncol", "int", "ncol", [12], 1)
+    cfgParser.addArgument("binFileName", "string", "binFileName", ["c.bin"], 1)
+
+    cfgParser.generateC()
+
+    cfg = cfgParser.interpret('demo.cfg')
+
     print(cfg)
+    print(cfg['src_row'])
+    print(cfg['src_col'])
--- testScript.m
+++ testScript.m
@@ -1,16 +1,16 @@
 clear
 clc
 
-a = parser();
-a.addArgument("observation_point", "int int float float", "obs_row obs_col golden weight", [1, 1, 1 1], 0)
-a.addArgument("src_point", "int int", "src_row src_col", [1, 1], 0);
-a.addArgument("nrow", "int", "nrow", [11], 1)
-a.addArgument("ncol", "int", "ncol", [12], 1)
-a.addArgument("binFileName", "string", "binFileName", "c.bin", 1)
+cfgParser = parser();
+cfgParser.addArgument("observation_point", "int int float float", "obs_row obs_col golden weight", [1, 1, 1 1], 0)
+cfgParser.addArgument("src_point", "int int", "src_row src_col", [1, 1], 0);
+cfgParser.addArgument("nrow", "int", "nrow", [11], 1)
+cfgParser.addArgument("ncol", "int", "ncol", [12], 1)
+cfgParser.addArgument("binFileName", "string", "binFileName", "c.bin", 1)
 
-a.generateC()
+cfgParser.generateC()
 
-configs = a.interpret("demo.cfg");
+configs = cfgParser.interpret("demo.cfg");
 
 obs_row = configs('obs_row')
 obs_col = configs('obs_col')
